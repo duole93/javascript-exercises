91b87cba5e75f2e7f4cceab5be18f559389e8698
Duong Le <duole93@live.com>
2023-11-01 14:04:16 -0500

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	let sum = 0;
+	if (
+		num1 < 0 ||
+		num2 < 0 ||
+		!Number.isInteger(num1) ||
+		typeof num2 !== "number"
+	)
+		return "ERROR";
+	//loop
+	let [min, max] = [num1, num2];
+	if (num1 > num2) [min, max] = [num2, num1];
 
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return sum;
+
+	//no loop
+	// return sum=(Math.abs(num1-num2)+1)*(num1+num2)/2;
 };
 
 // Do not edit below this line
