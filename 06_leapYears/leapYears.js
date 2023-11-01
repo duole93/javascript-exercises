d1a35bbed1ed07d28f92fa97d1da979eee43827b
Duong Le <duole93@live.com>
2023-11-01 14:20:14 -0500

    complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+// divsible by 4
+// !divisible by 100 && divisible by 400
 
+
+const leapYears = function(year) {
+    if(year%4===0&&(year%100!==0 || year%400===0 ))
+        return true;
+    return false;
 };
 
 // Do not edit below this line
