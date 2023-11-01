4f4cf6d75c176313a5f8b109b5471b4e2b10a233
Duong Le <duole93@live.com>
2023-11-01 14:39:52 -0500

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+ // return Number(((fahrenheit-32)/1.8).toFixed(1));
+ return Math.round((fahrenheit-32)/1.8*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //return fahrenheit=Number((celsius*1.8+32).toFixed(1));
+  return Math.round((celsius*1.8+32)*10)/10;
 };
 
 // Do not edit below this line
