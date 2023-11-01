5fb8fc6c64735fa64b8bb07b03917e9b1fbc6ead
Duong Le <duole93@live.com>
2023-11-01 14:05:41 -0500

    fix test.skip

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str , num) {
+    if(num <0){
+        return "ERROR";
+    }
+    else{
+        let finalStr='';
+        for (let i=0; i< num; i++){
+            finalStr+=str;
+        }
+        return finalStr;
+    }
 };
 
 // Do not edit below this line
