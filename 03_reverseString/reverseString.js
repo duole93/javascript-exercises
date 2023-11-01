56fe7cfef52c64632fbfebe20d6a91b3a5afdd18
Duong Le <duole93@live.com>
2023-10-31 20:53:11 -0500

    complete 02, 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // let finalStr='';
+    // let i=str.length-1;
+    // while (i>=0){
+    //     finalStr+=str[i];
+    //     i--;
+    // }
+    // return finalStr;
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
