fefccafd431499659615f7324664d23423b0ee03
Duong Le <duole93@live.com>
2023-11-21 18:42:39 -0600

    08, 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    
+	if (str.length === 0) {
+		return false;
+	}
+	// let newString = [...str.match(/[a-zA-Z0-9]/g)].join('').toLowerCase();
+    // console.log('newString :>> ', newString);
+    // let newReverse = [... newString].reverse().join('');
+    // console.log('newReverse :>> ', newReverse);
+	// return newString === newReverse;
+    let newStr = str.toLowerCase().replace(/[^a-z0-9]/g,"");
+    return newStr.split("").reverse().join("")===newStr
 };
 
 // Do not edit below this line
