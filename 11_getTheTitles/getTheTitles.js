bba9e2672bae676373e634cd4a2425391fa42589
Duong Le <duole93@live.com>
2023-11-22 21:13:05 -0600

    11_getTheTitle

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(books) {
+if (books.length<1){
+    return;
+}
+else{
+    return books.map((book)=>book.title);
+}
+}
 // Do not edit below this line
 module.exports = getTheTitles;
